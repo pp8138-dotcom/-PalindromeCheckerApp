11e20d4c287258f8b2867451947fd6820da843f6
pp8138-dotcom <pp8138@srmist.edu.in>
2026-03-02 10:17:27 +0530

    UC6: Queue and Stack based palindrome checker implemented

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,34 +1,40 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Palindrome Checker App - UC5 (Stack Based)");
-        System.out.println("-------------------------------------------");
+        System.out.println("=====================================");
+        System.out.println("   Palindrome Checker App - UC6");
+        System.out.println("=====================================");
 
         String original = "madam";
 
         Stack<Character> stack = new Stack<>();
-
-        // Push characters into stack
+        Queue<Character> queue = new LinkedList<>();
         for (int i = 0; i < original.length(); i++) {
-            stack.push(original.charAt(i));
+            char ch = original.charAt(i);
+            stack.push(ch);
+            queue.add(ch);
         }
 
-        // Pop characters to build reversed string
-        String reversed = "";
-
+        boolean isPalindrome = true;
         while (!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
         }
-        if (original.equals(reversed)) {
+
+        if (isPalindrome) {
             System.out.println(original + " is a Palindrome.");
         } else {
             System.out.println(original + " is NOT a Palindrome.");
         }
 
-        System.out.println("-------------------------------------------");
+        System.out.println("=====================================");
         System.out.println("Program Ended.");
     }
 }
